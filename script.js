41c194bdba529da4a692be25edd7e9dcf554dc77
ikizoglu <ikizoglukemal@gmail.com>
2024-08-26 00:09:48 +0300

    updated js file

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Bu bir yorum satırıdır. 
+
+/* Why Study JavaScript?
+JavaScript is one of the 3 languages all web developers must learn:
+
+1. HTML to define the content of web pages
+
+2. CSS to specify the layout of web pages
+
+3. JavaScript to program the behavior of web pages
+*/
